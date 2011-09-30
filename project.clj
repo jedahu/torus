cd3a12b6b7723f739be24343b45cb76c08f1fdd8
Jeremy Hughes <jedahu@gmail.com>
2011-09-30 16:09:33 +1300

    release 0.0.4

--- project.clj
+++ project.clj
@@ -1,21 +1,14 @@
 (defproject
-  me.panzoo/torus "0.0.4-SNAPSHOT"
+  me.panzoo/torus "0.0.4"
 
   :description "Clojurescript web application library"
 
   :dependencies
   [[org.clojure/clojure "1.3.0"]
    [clj-webdriver "0.2.14"]
-   [cljs-compiler "0.0.1"]]
+   [cljs-compiler "0.0.3"]]
 
   :exclusions
   [org.apache.ant/ant]
 
-  :source-path "cljs/src"
-
-  :cljs
-  {:compile-path "cljs/src"
-   :compile-opts
-   {:output-dir "out"
-    :output-to "out/torus.js"
-    :optimizations :simple}})
+  :source-path "cljs/src")
