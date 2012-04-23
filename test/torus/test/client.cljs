07c7d40010b6520e448ec84251a1c8c4e5e164e6
Jeremy Hughes <jedahu@gmail.com>
2012-04-24 00:59:55 +1200

    refactor tests

--- test/torus/test/client.cljs
+++ test/torus/test/client.cljs
@@ -10,111 +10,91 @@
   (:use-macros
     [menodora :only (defsuite describe should should* expect)]))
 
+(defn start-torus
+  [& {:as responses}]
+  (t/start
+    (fn [req]
+      (get responses (-> req :location :pathname)))))
+
 (defsuite core-tests
   (describe "client operation"
-    :let [tmap (atom nil)]
-    :pre (reset!
-           tmap
-           (t/start
-             (fn [req]
-               (condp = (-> req :location :pathname)
-                 "/"
-                 {:id "default"
-                  :html "<body>default</body>"}
-
-                 "/with-title"
-                 {:id "simple"
-                  :html (str "<html><head><title>Simple</title></head>"
-                             "<body>Hello<body></html>")}
-
-                 "/without-title"
-                 {:id "no-title"
-                  :html (str "<html><head></head>"
-                             "<body>Titleless</body></html>")}
-
-                 "/with-resp-title"
-                 {:id "resp-title"
-                  :html (str "<head><title>Not seen</title></head>")
-                  :title "Response"}))))
-    :post (do
-            (swap! tmap t/stop)
-            (. js/history pushState nil nil "/"))
-    (should "start"
-      (expect truthy @tmap))
     (should* "install template"
-      (events/listenOnce
-        (:event-target @tmap) te/INSTALLED
-        (fn [evt]
-          (expect eq "Simple" (. js/document -title))
-          (expect eq "Hello" (.. js/document -body -innerText))
-          (<done>)))
-      (t/change-path @tmap "/with-title"))
+      (let [tmap (start-torus
+                   "/with-title"
+                   {:id "hello"
+                    :html (str "<head><title>Simple</title></head>"
+                               "<body>Hello</body>")})]
+        (events/listenOnce
+          (:event-target tmap) te/INSTALLED
+          (fn [evt]
+            (expect eq "Simple" (. js/document -title))
+            (expect eq "Hello" (.. js/document -body -innerText))
+            (t/stop tmap)
+            (<done>)))
+        (t/change-path tmap "/with-title")))
     (should* "add hostname title if none given"
-      (events/listenOnce
-        (:event-target @tmap) te/INSTALLED
-        (fn [evt]
-          (expect eq "localhost" (. js/document -title))
-          (expect eq "Titleless" (.. js/document -body -innerText))
-          (<done>)))
-      (t/change-path @tmap "/without-title"))
+      (let [tmap (start-torus
+                   "/without-title"
+                   {:id "no-title"
+                    :html (str "<html><head></head>"
+                               "<body>Titleless</body></html>")})]
+        (events/listenOnce
+          (:event-target tmap) te/INSTALLED
+          (fn [evt]
+            (expect eq "localhost" (. js/document -title))
+            (expect eq "Titleless" (.. js/document -body -innerText))
+            (t/stop tmap)
+            (<done>)))
+        (t/change-path tmap "/without-title")))
     (should* "replace doc title with response title"
-      (events/listenOnce
-        (:event-target @tmap) te/INSTALLED
-        (fn [evt]
-          (expect eq "Response" (. js/document -title))
-          (<done>)))
-      (t/change-path @tmap "/with-resp-title")))
+      (let [tmap (start-torus
+                   "/with-resp-title"
+                   {:id "resp-title"
+                    :html (str "<head><title>Not seen</title></head>")
+                    :title "Response"})]
+            (events/listenOnce
+              (:event-target tmap) te/INSTALLED
+              (fn [evt]
+                (expect eq "Response" (. js/document -title))
+                (t/stop tmap)
+                (<done>)))
+            (t/change-path tmap "/with-resp-title"))))
 
   (describe "server operation"
-    :let [tmap (atom nil)]
-    :pre (reset!
-           tmap
-           (t/start
-             (fn [req]
-               (condp = (-> req :location :pathname)
-                 "/"
-                 {:id "default"
-                  :html "<body>default</body>"}
-
-                 "/server-hello"
-                 {:id "server hello"
-                  :html "/template/hello-world.html"}
-
-                 "/missing-template"
-                 {:id "missing"
-                  :html "/template/not-found.html"}))))
-    ;:post (do
-    ;        (events/listenOnce
-    ;          (:event-target @tmap) te/INSTALLED
-    ;          (fn [evt]
-    ;            (swap! tmap t/stop)
-    ;            (<done>)))
-    ;        (t/change-path @tmap "/"))
-    :post (do
-            (swap! tmap t/stop)
-            (. js/history pushState nil nil "/"))
+    :post (. js/history pushState nil nil "/")
     (should* "install template"
-      (events/listenOnce
-        (:event-target @tmap) te/INSTALLED
-        (fn [evt]
-          (expect eq "Hello World" (. js/document -title))
-          (expect eq "Hello World" (.. js/document -body -innerText))
-          (<done>)))
-      (t/change-path @tmap "/server-hello"))
+      (let [tmap (start-torus
+                   "/server-hello"
+                   {:id "server hello"
+                    :html "/template/hello-world.html"})]
+        (events/listenOnce
+          (:event-target tmap) te/INSTALLED
+          (fn [evt]
+            (expect eq "Hello World" (. js/document -title))
+            (expect eq "Hello World" (.. js/document -body -innerText))
+            (t/stop tmap)
+            (<done>)))
+        (t/change-path tmap "/server-hello")))
     (should* "handle 404s gracefully"
-      (events/listenOnce
-        (:event-target @tmap) te/INSTALLED
-        (fn [evt]
-          (expect eq "INSTALLED" "event should not be dispatched")
-          (events/removeAll {:event-target @tmap})
-          (<done>)))
-      (events/listenOnce
-        (:event-target @tmap) te/NOT-FOUND
-        (fn [evt]
-          (expect truthy "NOT-FOUND event should be dispatched")
-          (expect eq "server hello" (:id @t/current-response))
-          (events/removeAll {:event-target @tmap})
-          (<done>)))
-        (t/change-path @tmap "/missing-template"))))
+      (let [tmap (start-torus
+                   "/missing-template"
+                   {:id "missing"
+                    :html "/template/not-found.html"})]
+        (events/listenOnce
+          (:event-target tmap) te/INSTALLED
+          (fn [evt]
+            (expect eq "INSTALLED" "event should not be dispatched")
+            (events/removeAll {:event-target tmap})
+            (t/stop tmap)
+            (<done>)))
+        (events/listenOnce
+          (:event-target tmap) te/NOT-FOUND
+          (fn [evt]
+            (expect truthy "NOT-FOUND event should be dispatched")
+            (expect eq "server hello" (:id @t/current-response))
+            (events/removeAll {:event-target tmap})
+            (t/stop tmap)
+            (<done>)))
+        (t/change-path tmap "/missing-template")))))
 
 ;;. vim: set lispwords+=defsuite,describe,should,should*,expect:
